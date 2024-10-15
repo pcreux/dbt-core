d0e339f5aef093c3f766b815386d0ac7292e39ac
Devon Fulcher <24593113+DevonFulcher@users.noreply.github.com>
2024-10-15 16:49:02 -0500

    Behavior change for mf timespinse without yaml config

--- tests/unit/contracts/graph/test_semantic_manifest.py
+++ tests/unit/contracts/graph/test_semantic_manifest.py
@@ -1,6 +1,14 @@
+from argparse import Namespace
+from unittest.mock import patch
+
 import pytest
 
+from core.dbt.artifacts.resources.v1.model import TimeSpine
+from core.dbt.contracts.graph.manifest import Manifest
+from core.dbt.contracts.graph.nodes import ModelNode
+from core.dbt.flags import set_from_args
 from dbt.contracts.graph.semantic_manifest import SemanticManifest
+from tests.unit.utils.manifest import metricflow_time_spine_model
 
 
 # Overwrite the default nods to construct the manifest
@@ -24,6 +32,21 @@ def metrics(
 
 
 class TestSemanticManifest:
+
     def test_validate(self, manifest):
-        sm_manifest = SemanticManifest(manifest)
-        assert sm_manifest.validate()
+        with patch("dbt.contracts.graph.semantic_manifest.get_flags") as patched_get_flags:
+            patched_get_flags.return_value.allow_mf_time_spines_without_yaml_configuration = True
+            sm_manifest = SemanticManifest(manifest)
+            assert sm_manifest.validate()
+
+    def test_allow_mf_time_spines_without_yaml_configuration(
+        self, manifest: Manifest, metricflow_time_spine_model: ModelNode
+    ):
+        with patch("dbt.contracts.graph.semantic_manifest.get_flags") as patched_get_flags, patch(
+            "dbt.contracts.graph.semantic_manifest.deprecations"
+        ) as patched_deprecations:
+            patched_get_flags.return_value.allow_mf_time_spines_without_yaml_configuration = False
+            manifest.nodes[metricflow_time_spine_model.unique_id] = metricflow_time_spine_model
+            sm_manifest = SemanticManifest(manifest)
+            assert sm_manifest.validate()
+            assert patched_deprecations.warn.call_count == 1
