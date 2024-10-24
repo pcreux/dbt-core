c018e9d5e9ad15de328e6111ef7eabf3302ef627
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-10-24 18:30:00 -0500

    Add new `ArtifactWritten` event

--- core/dbt/events/types.py
+++ core/dbt/events/types.py
@@ -1188,6 +1188,19 @@ class DepsScrubbedPackageName(WarnLevel):
 
 
 # =======================================================
+# P - Artifacts
+# =======================================================
+
+
+class ArtifactWritten(DebugLevel):
+    def code(self):
+        return "P001"
+
+    def message(self) -> str:
+        return f"Wrote artifact {self.artifact_type} to {self.artifact_path}"
+
+
+# =======================================================
 # Q - Node execution
 # =======================================================
 
